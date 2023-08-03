1b3c12218964de386c58d6d670571dfac6014fe0
Weoice <84687104+Weoice@users.noreply.github.com>
2023-08-03 14:41:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function Bed() {
+
+window.location = "bedarea.html"
+
+}
+
+function Setu() {
+
+window.location = "setup.html"
+
+}
+
+function Living() {
+
+window.location = "living.html"
+
+}
+
+function Closet() {
+
+window.location = "closet.html"
+
+}
--- setup.js
+++ setup.js
@@ -1,37 +1,55 @@
+
 img = "";
-status = ""; 
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('Setup.jpg');
+}
+
 
 function setup() {
-
-canvas = createCanvas(648, 420);
-canvas.center();
-object_detector = ml5.objectDetector('cocossd', modelLoaded);
-document.getElementById("status").innerHTML = "status: detecting objects"
-
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function modelLoaded() {
-
-console.log("model Loaded");
-status = "true";
-object_detector.detect(img, gotResult);
-
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
 }
 
 function gotResult(error, results) {
-
-if(error) {
-
-console.log(error);
-
-
-}
-
-else {
-
-console.log(results);
-
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
 }
 
 
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+      if(status != "")
+      {
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
 }
+
+function Back4() {
+
+  window.location = "index.html";
+  
+  }
